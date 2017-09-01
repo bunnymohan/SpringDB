174f80850b93fc869c5f71c85a03d2b10dfa5e6e
bunnymohan <mohansai.ijjada@gmail.com>
2017-09-01 03:04:23 -0500

    first commit

---
+++ src/main/java/netgloo/controllers/UserControllerTest.java
@@ -0,0 +1,29 @@
+package netgloo.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserControllerTest {
+
+	@Test
+	public void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetByEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateUser() {
+		fail("Not yet implemented");
+	}
+
+}
